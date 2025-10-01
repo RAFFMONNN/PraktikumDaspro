cd2eed12c14c1152b629f7b297d7f42aa0234c49
RAFFMONNN <rafaalrabbani08@gmail.com>
2025-10-01 10:22:36 +0700

    TUGAS 1

--- jobsheet5/IfCetakKRS21.java
+++ jobsheet5/IfCetakKRS21.java
@@ -9,14 +9,11 @@ public class IfCetakKRS21 {
         System.out.println("--- Cetak KRS Siakad ---");
         System.out.println("Apakah UKT sudah lunas? (true/false)");
         boolean uktLunas = sc.nextBoolean();
-
-        if(uktLunas) {
-            System.out.println("Pembayaran UKT terverifikasi");
-            System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA ");
-        } else {
-                System.out.println("Registrasi ditolak. Silahkan lunasi UKT terlebih dahulu");
-
-        }
+        
+        String ukt = uktLunas ? "Pembayaran UKT Terverifikasi" : "Registrasi Ditolak";
+        System.out.println(ukt);
+        
+        
             
     }
 }
