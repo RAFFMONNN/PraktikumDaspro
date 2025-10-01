30d333846e05c754c52d8965fa6841d9aae99ddb
RAFFMONNN <rafaalrabbani08@gmail.com>
2025-10-01 07:44:17 +0700

    Percobaan 1

---
+++ IfCetakKRS21.java
@@ -0,0 +1,19 @@
+package PraktikumDaspro;
+
+import java.util.Scanner;
+public class IfCetakKRS21 {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("--- Cetak KRS Siakad ---");
+        System.out.println("Apakah UKT sudah lunas? (true/false)");
+        boolean uktLunas = sc.nextBoolean();
+
+        if(uktLunas) {
+            System.out.println("Pembayaran UKT terverikasi");
+            System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
+        }
+    }
+    
+}
